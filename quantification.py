0aadfe362559b583fede8fd804a3995ade2b7438
Kevin Cao <kev.j.cao@gmail.com>
2023-02-01 18:50:23 -0800

    Update quantification.py
    tweaks 2023-01-31

--- quantification.py
+++ quantification.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 # Install cellpose if needed
-#import pip
-#pip.main(['install', 'cellpose'])
+# import pip
+# pip.main(['install', 'cellpose'])
 
 import matplotlib.pyplot as plt
 from cellpose import models, io, plot, utils
@@ -16,19 +16,25 @@ import os
 root = tk.Tk()
 root.withdraw()
 image_path = filedialog.askopenfilename()
+path, filename = os.path.split(image_path)
+
+# locate folder
+#root = tk.Tk()
+#root.withdraw()
+#root.directory = filedialog.askdirectory()
 
 # view image
 img = io.imread(image_path)
-plt.figure(figsize=(5,5))
+plt.figure(figsize=(5, 5))
 plt.imshow(img)
 plt.axis('off')
 plt.tight_layout()
 plt.show()
 
-# define cellpose model; channels=[0,0] for grayscale image
+# define cellpose model; channels=[0, 0] for grayscale image
 model = models.Cellpose(gpu=False, model_type='cyto')
 img = io.imread(image_path)
-masks, flows, styles, diams = model.eval(img, diameter=None, channels=[0,0],
+masks, flows, styles, diams = model.eval(img, diameter=None, channels=[0, 0],
                                          flow_threshold=0.4, do_3D=False)
 
 # save cellpose output as _seg.npy
@@ -40,43 +46,31 @@ outlines = utils.outlines_list(masks)
 io.outlines_to_text(base, outlines)
 
 # plot segmentation, mask, and pose outputs
-fig1 = plt.figure(figsize=(12,5))
-plot.show_segmentation(fig1, img, masks, flows[0], channels=[0,0])
+fig1 = plt.figure(figsize=(12, 5))
+plot.show_segmentation(fig1, img, masks, flows[0], channels=[0, 0])
 plt.tight_layout()
+seg_file = filename.replace(".tif", ".png")
+plt.savefig(path + '/cellpose_output/' + seg_file)
 plt.show()
+
 
 # plot image with outlines overlaid in red
 np_seg = re.sub('.tif$', '_seg.npy', image_path)
 dat = np.load(np_seg, allow_pickle=True).item()
 outlines = utils.outlines_list(dat['masks'])
-fig2 = plt.figure(figsize=(5,5))
+fig2 = plt.figure(figsize=(5, 5))
 plt.imshow(dat['img'])
 for o in outlines:
-    plt.plot(o[:,0], o[:,1], linewidth=0.5, color='r')
+    plt.plot(o[:, 0], o[:, 1], linewidth=0.5, color='r')
 plt.axis('off')
 plt.tight_layout()
 plt.show()
 
+
 # measure mvg of segmented ROIs
 rois_n = np.zeros(len(outlines))
-for i in range(1, len(outlines)+1):
+for i in range(1, len(outlines) + 1):
     f_mask = np.isin(masks, i)
-    f_slice = f_mask*img
-    f_mean = f_slice[f_slice !=0].mean()
-    rois_n[i-1] = f_mean
-
-rois_mean = np.mean(rois_n)
-rois_std = np.std(rois_n)
-
-fig3 = plt.figure(figsize=(1,5))
-plt.bar('test', rois_n)
-plt.tight_layout()
-plt.show()
-
-
-
-
-
-
-
-
+    f_slice = f_mask * img
+    f_mean = f_slice[f_slice != 0].mean()
+    rois_n[i - 1] = f_mean
